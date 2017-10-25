fe2560934eb697b56c6644c24c76785e8a384cef
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-25 01:02:54 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(n)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(n)
+  until n == 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
