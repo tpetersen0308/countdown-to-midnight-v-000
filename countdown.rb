15a51a7aaea2d2337891e74fea06d8ca6cb1ac7b
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-25 01:03:22 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  until n == 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  until n == 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
